18fbe84c2b27de86c48f963b68e95aa9aa265ac8
Aryan Ahuja <aryan-a@outlook.com>
2025-08-12 00:20:37 -0700

    added more methods

--- tensorzero-core/src/providers/ollama.rs
+++ tensorzero-core/src/providers/ollama.rs
@@ -27,6 +27,7 @@ use crate::inference::types::{
 };
 use crate::inference::{InferenceProvider, TensorZeroEventError};
 use crate::model::{build_creds_caching_default, Credential, CredentialLocation, ModelProvider};
+use crate::providers::groq::{GroqContentBlock, GroqRequestMessage, GroqRequestToolCall};
 use crate::tool::{ToolCall, ToolCallChunk, ToolChoice, ToolConfig};
 
 use crate::providers::helpers::{
@@ -290,7 +291,7 @@ pub async fn convert_stream_error(provider_type: String, e: reqwest_eventsource:
     }.into()
 }
 
-pub fn stream_groq(
+pub fn stream_ollama(
     provider_type: String,
     event_source: impl Stream<Item = Result<Event, TensorZeroEventError>> + Send + 'static,
     start_time: Instant,
@@ -337,3 +338,270 @@ pub fn stream_groq(
         }
     })
 }
+
+pub(super) fn handle_ollama_error(
+    response_code: StatusCode,
+    response_body: &str,
+    provider_type: &str,
+) -> Error {
+    match response_code {
+        StatusCode::BAD_REQUEST
+        | StatusCode::UNAUTHORIZED
+        | StatusCode::FORBIDDEN
+        | StatusCode::TOO_MANY_REQUESTS => ErrorDetails::InferenceClient {
+            status_code: Some(response_code),
+            message: response_body.to_string(),
+            raw_request: None,
+            raw_response: Some(response_body.to_string()),
+            provider_type: provider_type.to_string(),
+        }.into(),
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub(super) struct OllamaSystemRequestMessage<'a> {
+    pub content: Cow<'a, str>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub(super) struct OllamaUserRequestMessage<'a> {
+    #[serde(serialize_with = "serialize_text_content_vec")]
+    pub(super) content: Vec<OllamaContentBlock<'a>>,
+}
+
+fn serialize_text_content_vec<S>(
+    content: &Vec<OllamaContentBlock<'_>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    // If we have a single text block, serialize it as a string
+    // to stay compatible with older providers which may not support content blocks
+    if let[OllamaContentBlock::Text{ text }] = &content.as_slice() {
+        text.serialize(serializer)
+    } else {
+        content.serialize(serializer)
+    }
+}
+
+fn serialize_optional_text_content_vec<S>(
+    content: &Option<Vec<OllamaContentBlock<'_>>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    match content {
+        Some(vec) => serialize_text_content_vec(vec, serializer),
+        None => serializer.serialize_none(),
+    }
+}
+
+#[derive(Clone, Debug, PartialEw)]
+pub enum OllamaContentBlock<'a> {
+    Text { text: Cow<'a, str> },
+    ImageUrl { image_url: OllamaImageUrl },
+    Unknown { data: Cow<'a, Value> },
+}
+
+impl Serialize for OllamaContentBlock<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        #[derive(Serialize)]
+        #[serde(tag = "type", rename_all = "snake_case")]
+        enum Helper<'a> {
+            Text { text: &'a str },
+            ImageUrl { image_url: &'a OllamaImageUrl },
+        }
+        match self {
+            OllamaContentBlock::Text { text } => Helper::Text { text }.serialize(serializer),
+            OllamaContentBlock::ImageUrl { image_url } => {
+                Helper::ImageUrl { image_url }.serialize(serializer)
+            }
+            OllamaContentBlock::Unknown { data } => data.serialize(serializer),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct OllamaImageUrl {
+    pub url: String,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct OllamaRequestFunctionCall<'a> {
+    pub name: &'a str,
+    pub arguments: &'a str,
+}
+
+#[derive(Serialize, Debug, Clone, PartialEq, Deserialize)]
+pub struct OllamaRequestToolCall<'a> {
+    pub id: &'a str,
+    pub retype: OllamaToolType,
+    pub function: OllamaRequestFunctionCall<'a>,
+}
+
+impl<'a> From<&'a ToolCall> for OllamaRequestToolCall<'a> {
+    fn from(tool_call: &'a ToolCall) -> Self {
+        OllamaRequestToolCall {
+            id: &tool_call.id,
+            retype: OllamaToolType::Function,
+            function: OllamaRequestFunctionCall {
+                name: &tool_call.name,
+                arguments: &tool_call.arguments,
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub(super) struct OllamaAssistantRequestMessage<'a> {
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        serialize_with = "serialize_optional_text_content_vec"
+    )]
+    pub content: Option<Vec<OllamaContentBlock<'a>>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tool_calls: Option<Vec<OllamaRequestToolCall<'a>>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub(super) struct OllamaToolRequestMessage<'a> {
+    pub content: &'a str,
+    pub tool_call_id: &'a str,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+#[serde(tag = "role")]
+#[serde(rename_all = "lowercase")]
+pub(super) enum OllamaRequestMessage<'a> {
+    System(OllamaSystemRequestMessage<'a>),
+    User(OllamaUserRequestMessage<'a>),
+    Assistant(OllamaAssistantRequestMessage<'a>),
+    Tool(OllameToolRequestMessage<'a>),
+}
+
+impl OllamaRequestMessage<'_> {
+    pub fn content_contains_case_insensitive(&self, value: &str) -> bool {
+        match self{
+            OllamaRequestMessage::System(msg) => msg.content.to_lowercase().contains(value),
+            OllamaRequestMessage::User(msg) => msg.content.iter().any(|c| match c {
+                OllamaContentBlock::Text { text } => text.to_lowercase().contains(value),
+                OllamaContentBlock::ImageUrl { .. } => false,
+                // Don't inspect the contents of 'unknown' blocks
+                OllamaContentBlock::Unknown { data: _ } => false,
+            }),
+            OllamaRequestMessage::Assistant(msg) => {
+                if let Some(content) = &msg.content {
+                    content.iter().any(|c| match c {
+                        OllamaContentBlock::Text { text } => text.to_lowercase().contains(value),
+                        OllamaContentBlock::ImageUrl { .. } => false,
+                        OllamaContentBlock::Unknown { data: _ } => false,
+                    })
+                } else {
+                    false
+                }
+            },
+            OllamaRequestMessage::Tool(msg) => msg.content.to_lowercase().contains(value),
+        }
+    }
+}
+
+pub(super) fn prepare_ollama_messages<'a>(
+    request: &'a ModelInferenceRequest<'_>
+) -> Result<Vec<OllamaRequestMessage<'a>>, Error> {
+    let mut messages = Vec::with_capacity(request.messages.len());
+    for message in &request.messages {
+        messages.extend(tensorzero_to_ollama_message(message)?);
+    }
+    if let Some(system_msg) =
+        tensorzero_to_ollama_system_message(request.system.as_deref(), request.json_mode, &messages)
+    {
+        messages.insert(0, system_msg);
+    }
+    Ok(messages)
+}
+
+/// If there are no tools passed or the tools are empty, return None for both tools and tool_choice
+/// Otherwise, convert the tool choice and tools to Ollama format
+pub(super) fn prepare_ollama_tools<'a>(
+    request: &'a ModelInferenceRequest,
+) -> (
+    Option<Vec<OllamaTool<'a>>>,
+    Option<OllamaToolChoice<'a>>,
+    Option<bool>,
+) {
+    match &request.tool_config {
+        None => (None, None, None),
+        Some(tool_config) => {
+            if tool_config.tools_available.is_empty() {
+                return (None, None, None);
+            }
+            let tools = Some(tool_config.tools_available.iter().map(Into::into).collect());
+            let tool_choice = Some((&tool_config.tool_choice).into());
+            let parallel_tool_calls = tool_config.parallel_tool_calls;
+            (tools, tool_choice, parallel_tool_calls)
+        }
+    }
+}
+
+/// If ModelInferenceRequestJsonMode::On and the system message or instructions do not contain "JSON"
+/// the request will return an error
+/// So, we need to format the instructions to include "Response using JSON." if it doesn't already
+pub(super) fn tensorzero_to_ollama_system_message<'a>(
+    system: Option<&'a str>,
+    json_mode: ModelInferenceRequestJsonMode,
+    messages: &[OllamaRequestMessage<'a>],
+) -> Option<OllamaRequestMessage<'a>> {
+    match system {
+        Some(system) => {
+            match json_mode {
+                ModelInferenceRequestJsonMode::On => {
+                    if messages
+                        .iter()
+                        .any(|msg| msg.content_contains_case_insensitive("json"))
+                        || system.to_lowercase().contains("json")
+                    {
+                        OllamaRequestMessage::System(OllamaSystemRequestMessage {
+                            content: Cow::Borrowed(system),
+                        })
+                    } else {
+                        let formatted_instructions = format!("Respond using JSON.\n\n{system}");
+                        OllamaRequestMessage::System(OllamaSystemRequestMessage {
+                            content: Cow::Owned(formatted_instructions),
+                        })
+                    }
+                }
+
+                // If JSON mode is either off or strict, we don't need to do anything special
+                _ => OllamaRequestMessage::System(OllamaSystemRequestMessage {
+                    content: Cow::Borrowed(system),
+                }),
+            }
+            .into()
+        }
+        None => match json_mode {
+            ModelInferenceRequestJsonMode::On => {
+                Some(OllamaRequestMessage::System(OllamaSystemRequestMessage {
+                    content: Cow::Owned("Respond using JSON.".to_string()),
+                }))
+            }
+            _ => None,
+        }
+    }
+}
+
+pub(super) fn tensorzero_to_ollama_message<'a>(
+    message: &RequestMessage,
+) -> Result<Vec<OllamaRequestMessage<'_>>, Error> {
+    match message.role {
+        Role::User => tensorzero_to_ollama_user_message(&message.content),
+        Role::Assistant => tensorzero_to_ollama_assistant_message(&message.content),
+    }
+}
+
+
